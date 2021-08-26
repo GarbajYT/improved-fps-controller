9680da99a4d1c2034e9f3e4d7bc29db5404fd82e
toivocat <87834761+toivocat@users.noreply.github.com>
2021-08-26 10:39:42 -0500

    Fix movement forward, backward, left, and right
    Movement was:
    
    	if Input.is_action_pressed("move_forward"):
    		direction -= transform.basis.z
    	elif Input.is_action_pressed("move_backward"):
    		direction += transform.basis.z
    	if Input.is_action_pressed("move_left"):
    		direction -= transform.basis.x
    	elif Input.is_action_pressed("move_right"):
    		direction += transform.basis.x
    
    But needed to be:
    
    	if Input.is_action_pressed("move_forward"):
    		direction += transform.basis.z
    	elif Input.is_action_pressed("move_backward"):
    		direction -= transform.basis.z
    	if Input.is_action_pressed("move_left"):
    		direction += transform.basis.x
    	elif Input.is_action_pressed("move_right"):
    		direction -= transform.basis.x

--- NewFPSCont.gd
+++ NewFPSCont.gd
@@ -47,13 +47,13 @@ func _physics_process(delta):
 		gravity_vec = Vector3.UP * jump
 	
 	if Input.is_action_pressed("move_forward"):
-		direction -= transform.basis.z
-	elif Input.is_action_pressed("move_backward"):
 		direction += transform.basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction -= transform.basis.z
 	if Input.is_action_pressed("move_left"):
-		direction -= transform.basis.x
-	elif Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction -= transform.basis.x
 	
 	direction = direction.normalized()
 	h_velocity = h_velocity.linear_interpolate(direction * speed, h_acceleration * delta)
